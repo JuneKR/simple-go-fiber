da540fc7592fd73e08ce71d50266596dd63fe774
Aekkaluk Panyacharoensri <d6600128@outsource.ktb.co.th>
2024-01-15 12:21:07 +0700

    add update book api

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func main() {
 	app.Get("/books", getBooks)
 	app.Get("/books/:id", getBook)
 	app.Post("/books", createBook)
+	app.Put("/books/:id", updateBook)
 
 	app.Listen(":8081")
 }
@@ -55,4 +56,27 @@ func createBook(c *fiber.Ctx) error {
 
 	books = append(books, *book)
 	return c.JSON(book)
+}
+
+func updateBook(c *fiber.Ctx) error {
+	bookId, err := strconv.Atoi(c.Params("id"))
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	bookUpdate := new(Book)
+	if err := c.BodyParser(bookUpdate); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	for i, book := range books {
+		if book.ID == bookId {
+			books[i].Title = bookUpdate.Title
+			books[i].Author = bookUpdate.Author
+			return c.JSON(books[i])
+		}
+	}
+
+	return c.SendStatus(fiber.StatusNotFound)
 }
